64adade440bd1ffccb69da3c77132b9202487356
bran-shin <bran@memolease.com>
2024-10-16 10:14:01 +0900

    project init

---
+++ movie/movie/movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieApp.swift
+//  movie
+//
+//  Created by Bran on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
