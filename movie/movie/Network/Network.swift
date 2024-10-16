4361024d62fb60f5d7da1d2bc4799a481e441c91
Bran9999 <cold929.dev@gmail.com>
2024-10-16 10:35:02 +0900

    feat: make network model

---
+++ movie/movie/Network/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  movie
+//
+//  Created by Bran on 10/16/24.
+//
+
+import Foundation
---
+++ movie/movie/Network/NetworkModel.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkModel.swift
+//  movie
+//
+//  Created by Bran on 10/16/24.
+//
+
+import Foundation
+
+struct TMDB미디어 {
+
+}
