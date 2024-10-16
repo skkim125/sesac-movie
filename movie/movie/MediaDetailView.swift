d14d1361b092b32f6fb9a2191a77db7f323b25b3
Bran <cold929@naver.com>
2024-10-16 14:14:38 +0900

    미디어 디테일뷰 구현

---
+++ movie/movie/MediaDetailView.swift
@@ -0,0 +1,15 @@
+//
+//  MediaDetailView.swift
+//  movie
+//
+//  Created by Bran on 10/16/24.
+//
+
+import SwiftUI
+
+struct MediaDetailView: View {
+  var body: some View {
+    Text("Media DetailView")
+  }
+}
+
