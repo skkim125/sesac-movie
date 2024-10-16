b9d7da1a95831e154cc2d75e8240b6adb98ff512
skkim125 <kthanks125@gmail.com>
2024-10-16 14:14:49 +0900

    미디어 디테일 뷰 구현

---
+++ movie/movie/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  movie
+//
+//  Created by 김상규 on 10/16/24.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text("MovieDetailView")
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
